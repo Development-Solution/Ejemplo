e27832a526d7980947045152bb53c63e7df88b4e
Wilmar Zapata <wavilla@stefanini.com>
2022-10-25 03:11:21 -0500

    creando router y sus respectivas rutas

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Root from './routes/root';
+import ErrorPage from './error-page';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import NuevoProducto from './routes/nuevo-producto';
+import ListaProductos from './components/lista-productos';
+import ListaVentas from './routes/lista-ventas'
+import NavBar from "./navbar"
+
+const router = createBrowserRouter([
+  {path: '/', element: <Root />, errorElement: <ErrorPage />},
+  {path: '/nuevo-producto', element: <NuevoProducto />},
+  {path: '/lista-productos', element: <ListaProductos />},
+  {path: '/lista-ventas', element: <ListaVentas />},
+  {path: '/default-react', element: <App />}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar></NavBar>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
